9a40d3497518c40783a632d39e0ad3077bd96f0a
Julaluk Paladkhwa <j.paladkhwa@gmail.com>
2022-06-10 07:57:02 +0700

    [updates]

--- src/App.js
+++ src/App.js
@@ -2,24 +2,6 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import Table from "./Table";
 
-// function myFunction() {
-//   let input, filter, table, tr, td, i, txtValue;
-//   input = document.getElementById("myInput");
-//   filter = input.value.toUpperCase();
-//   table = document.getElementById("myTable");
-//   tr = table.getElementsByTagName("tr");
-//   for (i = 0; i < tr.length; i++) {
-//     td = tr[i].getElementsByTagName("td")[0];
-//     if (td) {
-//       txtValue = td.textContent || td.innerText;
-//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//         tr[i].style.display = "";
-//       } else {
-//         tr[i].style.display = "none";
-//       }
-//     }
-//   }
-// }
 function App() {
   const [item, setItem] = useState([]);
   const [query, setQuery] = useState('');
